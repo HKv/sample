0fdfe573ee5cfffa2809fbbcf9f5f4df00f53198
HKv <kavayakiintheskywithdiamond@gmail.com>
2014-02-02 22:35:25 +0900

    Added rails\_12factor gem and updated Gemfile.lock

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sample::Application.config.secret_key_base = '8bc66fa35234849979e43dfd933cff9a1b72a645a1e5d22ae3c2d3bec19b6bb51596258da2d129aab2d325dd7e95d9910045f65a022e8d72301b0b980189dc60'
